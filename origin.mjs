c5a4c2d2ea04e311dcbc32708e09d18bd53ff1f5
Brian LEE <93697790+CodyMan0@users.noreply.github.com>
2023-03-13 12:11:19 +0900

    add : 3.13 일반적인 배열과 연결 리스트 구현

---
+++ origin.mjs
@@ -0,0 +1,50 @@
+import { Node, LinkedList } from "./linkedList/linkedList.mjs";
+
+let node1 = new Node(1);
+let node2 = new Node(2);
+let node3 = new Node(3);
+
+node1.next = node2;
+node2.next = node3;
+
+console.log(node1.data);
+console.log(node1.next.data);
+console.log(node1.next.next.data);
+
+let list = new LinkedList();
+console.log("======== insertAt");
+list.insertAt(0, 0);
+list.insertAt(1, 1);
+list.insertAt(2, 2);
+list.insertAt(3, 3);
+list.insertAt(4, 4);
+list.printAll();
+
+console.log("======== clear");
+list.clear();
+list.printAll();
+
+console.log("======== insertLast");
+list.insertLast(0);
+list.insertLast(2);
+list.insertLast(1);
+list.printAll();
+
+console.log("======== delete");
+list.deleteAt(0);
+list.printAll();
+
+console.log("======== deleteLast");
+list.deleteLast();
+list.printAll();
+
+console.log("======== getNodeAt");
+list.insertLast(1);
+list.insertLast(2);
+list.insertLast(5);
+list.insertLast(10);
+
+let secondNode = list.getNodeAt(2);
+
+console.log(secondNode);
+list.printAll();
